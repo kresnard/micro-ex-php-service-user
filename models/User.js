b44b356c3031a794a3938d1aadbf2c8eb039f1e0
kresnard <kresnard@gmail.com>
2023-07-06 14:29:31 +0700

    register endpoint

---
+++ models/User.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            values: ['admin', 'student'],
+            defaultValues: 'student'
+        },
+        avatar: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        profession: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+            allowNull: false,
+        }
+    },{
+        tableName: 'users',
+        timestamps: true
+    });
+
+    return User;
+}
